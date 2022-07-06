116e1e0d76063427fc13cfa24a8ddea5124a8370
herdal06 <h.erdal721@gmail.com>
2022-07-06 18:22:08 +0300

    Add design for character details

--- app/src/main/java/com/example/harrypottermvvm/model/CharacterResponseItem.kt
+++ app/src/main/java/com/example/harrypottermvvm/model/CharacterResponseItem.kt
@@ -1,7 +1,10 @@
 package com.example.harrypottermvvm.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class CharacterResponseItem(
     @SerializedName("actor")
     val actor: String?,
@@ -11,4 +14,16 @@ data class CharacterResponseItem(
     val name: String?,
     @SerializedName("house")
     val house: String?,
-)
+    @SerializedName("gender")
+    val gender: String?,
+    @SerializedName("yearOfBirth")
+    val yearOfBirth: String?,
+    @SerializedName("hairColor")
+    val hairColor: String?,
+    @SerializedName("eyeColor")
+    val eyeColor: String?,
+    @SerializedName("wizard")
+    val wizard: Boolean?,
+    @SerializedName("alive")
+    val alive: Boolean?,
+) : Parcelable
